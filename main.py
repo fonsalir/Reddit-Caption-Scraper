f8ae75d49781286344b484abad196d6c241ea823
Matthe Davenport <mdav121503@gmail.com>
2023-01-24 10:29:36 -0600

    fixed the list containing captions

--- main.py
+++ main.py
@@ -3,7 +3,16 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
-#from colorama import Fore, Back, Style
+import sys
+
+#how many reddit titles will be scrapped
+requested_amount = input("how many?: ")
+try:
+    int_requested_amount = int(requested_amount)
+except ValueError:
+    sys.exit("Invalid")
+
+print(int_requested_amount)
 
 #gets the path of my chrome driver
 service = Service(executable_path="C:\Program Files (x86)\chromedriver")
@@ -15,17 +24,26 @@ driver.get(SITE)
 html = driver.find_element(By.TAG_NAME,'html')
 h3tags = driver.find_elements(By.XPATH, "//h3")
 
+
 captions = []
-while True:
+loop = True
+while loop:
     html.send_keys(Keys.PAGE_DOWN)
     time.sleep(0.5)
     html.send_keys(Keys.PAGE_DOWN)
     h3tags = driver.find_elements(By.XPATH, "//h3")
-    captions = {"",}
+    captions = set()
     for tag in h3tags:
         captions.add(tag.text)
-    if len(captions) >= 10:
-        break
+        if len(captions) >= int_requested_amount:
+            loop = False
 
-for caption in captions:
-    print(caption)
+captions = list(captions)
+if "" in captions:
+    captions.remove("")
+if len(captions) >=  int_requested_amount:
+    for caption in range(int_requested_amount):
+        print(captions[caption])
+else:
+    for caption in captions:
+        print(caption)
